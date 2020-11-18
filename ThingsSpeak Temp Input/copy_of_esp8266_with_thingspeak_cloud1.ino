e2c78365b133184e5c66f685d73cf9b886c789d5
PratikGarai <pratikgarai0208@gmail.com>
2020-11-18 14:44:53 +0530

    Added things speak example

---
+++ ThingsSpeak Temp Input/copy_of_esp8266_with_thingspeak_cloud1.ino
@@ -0,0 +1,61 @@
+//Code credits to :  Technomekanics:)
+String ssid     = "Simulator Wifi";  // SSID to connect to
+String password = ""; // Our virtual wifi has no password 
+String host     = "api.thingspeak.com"; // Open Weather Map API
+const int httpPort   = 80;
+String url     = "/update?api_key=EFB42YJN2YQ7FRSF&field1=";
+
+int setupESP8266(void) {
+  // Start our ESP8266 Serial Communication
+  Serial.begin(115200);   // Serial connection over USB to computer
+  Serial.println("AT");   // Serial connection on Tx / Rx port to ESP8266
+  delay(10);        // Wait a little for the ESP to respond
+  if (!Serial.find("OK")) return 1;
+    
+  // Connect to 123D Circuits Simulator Wifi
+  Serial.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"");
+  delay(10);        // Wait a little for the ESP to respond
+  if (!Serial.find("OK")) return 2;
+  
+  // Open TCP connection to the host:
+  Serial.println("AT+CIPSTART=\"TCP\",\"" + host + "\"," + httpPort);
+  delay(50);        // Wait a little for the ESP to respond
+  if (!Serial.find("OK")) return 3;
+  
+  return 0;
+}
+
+void anydata(void) {
+  
+  int temp = map(analogRead(A0),20,358,-40,125);
+  
+  // Construct our HTTP call
+  String httpPacket = "GET " + url + String(temp) + " HTTP/1.1\r\nHost: " + host + "\r\n\r\n";
+  int length = httpPacket.length();
+  
+  // Send our message length
+  Serial.print("AT+CIPSEND=");
+  Serial.println(length);
+  delay(10); // Wait a little for the ESP to respond if (!Serial.find(">")) return -1;
+
+  // Send our http request
+  Serial.print(httpPacket);
+  delay(10); // Wait a little for the ESP to respond
+  if (!Serial.find("SEND OK\r\n")) return;
+  
+  
+}
+
+
+void setup() {
+  
+  setupESP8266();
+               
+}
+
+void loop() {
+  
+ anydata();
+  
+  delay(1000);
+}
